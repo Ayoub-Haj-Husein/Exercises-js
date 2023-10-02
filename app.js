4b6e9580fb3c69e70d62e6e6b0b7bff288bc1b47
Ayoub-Haj-Husein <ayoubabdulrahmanhusein@gmail.com>
2023-10-03 00:17:20 +0300

    Solve exsercies (7-13)

--- app.js
+++ app.js
@@ -73,9 +73,7 @@ alert(
 );
 
 // Q6
-alert(
-  "Q6: If you have a “num” variable,  Your task is to print:"
-);
+alert("Q6: If you have a “num” variable,  Your task is to print:");
 let num = +prompt("please enter your number");
 switch (num) {
   case 1:
@@ -117,3 +115,82 @@ switch (num) {
   default:
     alert("PLEASE TRY AGAIN");
 }
+
+// Q7
+alert(
+  "Q7: Use a loop to display the numbers 0 through 5, each in a separate alert window."
+);
+for (let i = 0; i <= 5; i++) {
+  alert(i);
+}
+
+// Q8
+alert(
+  "Q8: Use a loop to display the numbers 0 through 5 in a single alert window"
+);
+let numbers = "";
+for (let i = 0; i <= 5; i++) {
+  numbers += `${i} `;
+}
+alert(numbers);
+
+// Q9
+alert(
+  "Q9: Use a loop to display the numbers in the range 0…20 that are multiples of 3."
+);
+let multiplesOf_3 = "";
+for (let i = 3; i >= 0 && i <= 20; ) {
+  multiplesOf_3 += `${i}, `;
+  i += 3;
+}
+alert(multiplesOf_3);
+
+// Q10
+alert(
+  "Q10: Prompt the user for a number in the range 0…100.  If the number is out of range, display an error message and prompt again until a valid number is entered.  Assume the user enters a number each time"
+);
+let number_0_100 = +prompt("please Enter a number");
+while (!(number_0_100 >= 0 && number_0_100 <= 100)) {
+  alert("Error => please Enter a valid number (0-100)");
+  number_0_100 = +prompt("please Enter a number");
+}
+alert(`Your Number Is ${number_0_100}`);
+
+// Q11
+alert(
+  "Q11: Repeat previous exercise, but this time allow for the possibility that the user enters something that is not a number.  Hint: the built in function isNaN(x) returns true if x is not a number.  It returns false if x is a number.  Sort of backwards, but that's what's available."
+);
+let number_Q_11;
+for (;;) {
+  number_Q_11 = +prompt("Please enter a number");
+  if (number_Q_11 === null || isNaN(number_Q_11)) {
+    alert("Invalid input. Please Enter A Valid Number.");
+  } else if (!isNaN(number_Q_11) && number_Q_11 >= 0 && number_Q_11 <= 100) {
+    alert(`You Number Is ${number_Q_11}`);
+    break;
+  } else {
+    alert("Number out of range. Please enter a number between (0-100)");
+  }
+}
+
+// Q12
+alert(
+  "Q12: Prompt for an integer, then display the sum of the integers from 0 through the number entered.  For example, if you enter 10, then display 55 which is the sum of 0 + 1 + 2 + 3 + 4 + 5 + 6 + 7 + 8 + 9 + 10"
+);
+let integerNumber = +prompt("Please enter a integer number");
+let sum_Q_12 = 0;
+for (let i = 0; i <= integerNumber; i++) {
+  sum_Q_12 += i;
+}
+alert(sum_Q_12);
+
+// Q13
+alert(
+  "Q13: Prompt for an integer, then display the average of the integers from 0 through the number entered.  For example, if you enter 10, then display 5 which is the average of   (0 + 1 + 2 + 3 + 4 + 5 + 6 + 7 + 8 + 9 + 10)/11."
+);
+let integerNumber_Q_13 = +prompt("Please enter a integer number");
+let sum_Q_13 = 0;
+for (let i = 0; i <= integerNumber_Q_13; i++) {
+  sum_Q_13 += i;
+}
+alert(sum_Q_13 / (integerNumber_Q_13 + 1));
